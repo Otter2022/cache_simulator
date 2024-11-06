a1cd37da9a53e8b56b17b62544102860a0243443
Otter2022 <ottoz@live.com>
2024-11-06 17:36:34 -0600

    added initial files

---
+++ cache.py
@@ -0,0 +1,10 @@
+# -s <cache size - KB> [ 8 to 8198 KB]
+# -b <block size> [ 8 bytes to 64 bytes ]
+# -a <associativity> [ 1, 2, 4, 8, 16 ]
+# -r <replacement policy> [ RR or RND ]  Implement one of these
+# -p <physical memory - MB> [ 1 MB to 4 GB ]
+# -u <% phys mem used> [ 0% to 100% ]
+# -n <Instr / Time Slice [ 0 to -1 ] -1 = max
+# -f <trace file name> [ name of text file with the trace ]
+# o You must accept 1, 2, or 3 trace files as input
+# o Each file will use a “-f “ to specify it 
